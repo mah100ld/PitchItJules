7b9eac489c5cfa0b789240e2d58512a61ce11755
Maximilian Horst <mahorst@deloitte.de>
2021-08-22 17:08:00 +0200

    Setup and developed first draft

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import PhraseGenerator from './PhraseGenerator/PhraseGenerator';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="container">
+        <div class="row align-items-center">
+          <div className="header-background">
+
+          </div>
+        </div>
+        <div class="row mt-5 pl-2 pr-2">
+          <PhraseGenerator />
+          </div>
+
+        <div className="row mt-5 ml-10 mr-10">
+        <iframe id="ytplayer" type="text/html" width="640" height="360"
+        src="https://www.youtube.com/watch?v=nUE841pCXtc"
+        frameborder="0"></iframe>
+        </div>
+      </div>
+        
     </div>
   );
 }
